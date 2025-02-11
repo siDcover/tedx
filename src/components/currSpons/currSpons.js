e6a3abe8b3399c98ed5c064564554a801f8c7016
sidd <borgavesiddharth@gmail.com>
2025-02-11 15:33:19 +0530

    first commit

--- src/components/currSpons/currSpons.js
+++ src/components/currSpons/currSpons.js
@@ -1,12 +1,14 @@
 import React from "react";
 import img1 from "../../assets/images/sponsors-2025/1.jpg";
 import img2 from "../../assets/images/sponsors-2025/2.jpg";
+import img3 from "../../assets/images/sponsors-2025/3.jpg";
 import "./currSpons.css";
 
 function Spons() {
   const sponsdata = [
     { id: 1, pic: img1, link: "https://www.kaalibilli.in/", name: "Kali Billi" },
     { id: 2, pic: img2, link: "https://kweenmedia.in/", name: "KWEEN Media" },
+    { id: 3, pic: img3, link: "https:/tiptophotel.in/", name: "Hotel Tip Top" },
 
     // { id: 5, pic: img5 , link:"https://www.monsterenergy.com/en-in/"},
   ];
